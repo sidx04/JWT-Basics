b186e2df0a5f8358de252771b3acd5fef6cb9ede
Onion-Man <siddpal04@gmail.com>
2022-12-07 14:55:09 +0530

    implementing jwt

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,5 +1,20 @@
+const jwt=require('jsonwebtoken')
+const CustomAPIError=require('../errors/custom-error')
+
 const login=async(req,res)=>{
-    res.send('login test')
+    const {username,password}=req.body
+    
+    if(!username||!password){
+        throw new CustomAPIError('please provide name and password...',400)
+    }    
+
+    const id=new Date().getDate() //normally provided by database
+
+    //payload should be small
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: '15d',
+      })
+    res.status(200).json({ msg: 'user created', token:token })
 }
 
 const dashboard=async(req,res)=>{
