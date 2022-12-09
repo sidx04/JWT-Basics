6833bbb41839650f77b792c4009c646a58a55034
Onion-Man <siddpal04@gmail.com>
2022-12-09 19:55:46 +0530

    updated error handling routes and middleware

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,9 +1,13 @@
-const CustomAPIError=require('../errors/custom-error')
+const {CustomAPIError}=require('../errors')
+const {StatusCodes}=require('http-status-codes')
+
 const errorHandlerMiddleware=(err,req,res,next)=>{
     if(err instanceof CustomAPIError){
         return res.status(err.statusCode).json({msg:err.message})
     }
-    return res.status(500).send('something went wrong, please try again later...')
+    return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send('something went wrong, please try again later...')
 }
 
 module.exports=errorHandlerMiddleware
